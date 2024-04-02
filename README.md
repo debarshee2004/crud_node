0b60655179c239523e42d6b0ab65be89f70a52bf
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2024-04-02 21:18:10 +0530

    Creating a small api

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # CRUD NODE
 
-## Project Setup
+To setup the project.
 
 ```bash
 npm init -y
 npm i mongoose express
+```
+
+To run the server.
+
+```bash
+node --watch index.js
 ```
--- index.js
+++ index.js
@@ -1,3 +1,44 @@
 // Importing the express library
-const express = require('express');
+const express = require("express");
+
+const fs = require("fs");
+const path = require("path");
+
 const app = express();
+
+const PORT = 3000;
+
+app.use(express.json());
+
+const dataFIlePath = path.join(__dirname, "data.json");
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.post("/items", (req, res) => {
+  const newItem = req.body;
+  fs.readFile(dataFIlePath, "utf8", (err, data) => {
+    if (err) {
+      console.error("Error :", err);
+      res.status(500).json({
+        message: "Server Error",
+      });
+    }
+    let items = JSON.parse(data);
+    items.push(newItem);
+
+    fs.writeFile(dataFIlePath, JSON.stringify(items), (err) => {
+      if (err) {
+        console.error("Error :", err);
+        res.status(500).json({
+          message: "Server Error",
+        });
+      }
+    });
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
